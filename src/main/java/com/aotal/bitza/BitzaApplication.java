3bc0339802d5e9931048d8c1a7c97c30f0b26626
William Kerrigan <william.kerrigan@aotal.com>
2017-07-26 13:12:50 +1200

    Add actuator

--- src/main/java/com/aotal/bitza/BitzaApplication.java
+++ src/main/java/com/aotal/bitza/BitzaApplication.java
@@ -1,9 +1,13 @@
 package com.aotal.bitza;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class BitzaApplication {
 
 	public static void main(String[] args) {
